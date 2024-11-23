2fd216c0aa0849a3a9dd68530ad05c005cfa259b
Miguel Vital <miguelvital05@gmail.com>
2024-11-22 22:12:59 -0300

    pegando os dados do user

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -2,16 +2,27 @@
 
 namespace app\controllers;
 
+use app\models\activerecord\FindBy;
+use app\models\User as UserModel;
+use Exception;
+
 class User
 {
     public $view;
     public array $data = [];
 
-    public function show($args)
+    public function show(array $args)
     {
+        $user = (new UserModel)->execute(new FindBy(field:'id',value:$args[0], fields:'id,firstName,lastName'));
+
+        if (!$user) {
+            throw new Exception('Usuário não encontrado');
+        }
+
         $this->view = 'user.php';
         $this->data = [
             'title' => 'Dados do Usuário',
+            'user'  => $user,
         ];
     }
 }
--- app/views/index.php
+++ app/views/index.php
@@ -5,11 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title> <?php echo $title ?> </title>
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
 </head>
 <body>
 
     <div class="container">
+
+        <section id="header">
+            <ul id="nav">
+                <li><a href="/">Início</a></li>
+                <li><a href="/signup">Sign Up</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+
+            <div>
+                Bem Vindo!
+            </div>
+        </section>
+
         <?php require VIEW_PATH.$this->controller->view ?>
     </div>
 </body>
