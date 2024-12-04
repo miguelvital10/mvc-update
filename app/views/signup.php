b924ff46210ca0edbace053997ee156ed31b9d18
Miguel Vital <miguelvital05@gmail.com>
2024-12-03 22:11:52 -0300

    feat(signup,firstName): add flash firstName

--- app/views/signup.php
+++ app/views/signup.php
@@ -1,9 +1,10 @@
 <form method="post" action="signup/store">
     <input type="text" name="firstName" placeholder="Nome" class="form-control" value="<?php echo old('firstName') ?>">
-
+    <?php echo flash('firstName'); ?>
+    
     <input type="text" name="lastName" placeholder="Sobrenome" class="form-control" value="<?php echo old('lastName')?>">
     <?php echo flash('lastName'); ?>
-        
+    
     <input type="email" name="email" placeholder="E-mail" class="form-control" value="<?php echo old('email')?>">
     <?php echo flash('email'); ?>
         
